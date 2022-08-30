a45859d8874ff832c4285dfbcaa8fb6706f73449
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2022-08-30 17:12:52 +0530

    Add files via upload

--- Inheritance/Progression/Progression_Tester.py
+++ Inheritance/Progression/Progression_Tester.py
@@ -13,21 +13,51 @@ from Fibonacci_Progression import FibonacciProgression
 if __name__ == '__main__':
     print('Default Progression:')
     Progression().print_progression(10)
+    print()
     
     print('Arithmetic Progression with increment 5:')
     ArithmeticProgression(5).print_progression(10)
+    print()
     
     print('Arithmetic Progression with start 2 and increment 5:')
     ArithmeticProgression(2, 5).print_progression(10)
+    print()
     
     print('Geometric Progression with default factor:')
     GeometricProgression().print_progression(10)
+    print()
     
     print('Geometric Progression with the factor 3:')
     GeometricProgression(3).print_progression(10)
+    print()
     
     print('Fibonacci Progression with default start values:')
     FibonacciProgression().print_progression(10)
+    print()
     
     print('Fibonacci Progression with default start values 4 and 6:')
     FibonacciProgression(4, 6).print_progression(10)
+        
+    '''
+    Output:
+        Default Progression:
+        0 1 2 3 4 5 6 7 8 9
+
+        Arithmetic Progression with increment 5:
+        5 6 7 8 9 10 11 12 13 14
+
+        Arithmetic Progression with start 2 and increment 5:
+        2 7 12 17 22 27 32 37 42 47
+
+        Geometric Progression with default factor:
+        1 2 4 8 16 32 64 128 256 512
+
+        Geometric Progression with the factor 3:
+        3 6 12 24 48 96 192 384 768 1536
+
+        Fibonacci Progression with default start values:
+        0 1 1 2 3 5 8 13 21 34
+
+        Fibonacci Progression with default start values 4 and 6:
+        4 6 10 16 26 42 68 110 178 288
+    '''
