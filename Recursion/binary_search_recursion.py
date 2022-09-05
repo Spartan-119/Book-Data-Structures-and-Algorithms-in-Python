ad7085469aefc8eb8c066014880c361e38800295
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2022-09-05 19:52:40 +0530

    Add files via upload

--- Recursion/binary_search_recursion.py
+++ Recursion/binary_search_recursion.py
@@ -1,3 +1,16 @@
+'''
+We consider three cases:
+• If the target equals data[mid], then we have found the item we are looking
+for, and the search terminates successfully.
+• If target < data[mid], then we recur on the first half of the sequence, that is,
+on the interval of indices from low to mid−1.
+• If target > data[mid], then we recur on the second half of the sequence, that
+is, on the interval of indices from mid+1 to high.
+An unsuccessful search occurs if low > high, as the interval [low,high] is empty.
+
+Time Complexity: O(logn)
+'''
+
 def binary_search(data, target, low, high):
     if low > high:
         return "The element is NOT present in the list."
@@ -10,6 +23,7 @@ def binary_search(data, target, low, high):
             # recur on the left portion of mid
             return binary_search(data, target, low, mid - 1)
         else:
+            # recur on the right portion of mid
             return binary_search(data, target, mid + 1, high)
 
 data = [1, 2, 5, 10, 40, 41]
