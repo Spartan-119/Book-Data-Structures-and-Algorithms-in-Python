e4ba3083b6cda54ae828c176ffa9e5750ad7ef76
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2022-09-12 00:35:14 +0530

    Add files via upload

--- Array Based Sequences/Dynamic_Array.py
+++ Array Based Sequences/Dynamic_Array.py
@@ -20,3 +20,31 @@ class DynamicArray:
         return number of elements stored in the array.
         '''
         return self._n
+    
+    def __getitem__(self, k):
+        '''
+        Returns an element at the index k.
+        '''
+        if not 0 <= k <= self._n:
+            raise IndexError('Invalid index!')
+        return self._A[k]
+
+    def _resize(self, c) -> None:              # private utility
+        '''
+        Resizing the internal size to capacity c.
+        '''
+        B = self._make_array(c)                 # new bigger array with capacity c.
+        for element in range(self._n):          # for each existing element in the previous array.
+            B[element] = self._A[element]       # assign the elements in the corresponding index of the new array.
+        self._A = B                             # assigning the pointers, meaning, use the bigger array from now on.
+        self._capacity = c                      # update the capacity.
+
+    
+    def append(self, obj) -> None:
+        '''
+        Add object at the end of the array.
+        '''
+        if self._n == self._capacity:           # not enough room
+            self._resize(2 * self._capacity)    # so double the capacity
+        self._A[self._n] = obj                  # add the obj at the end of the array
+        self._n += 1                            # increase the length of the array
